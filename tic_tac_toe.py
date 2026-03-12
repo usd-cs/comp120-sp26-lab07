fbd3fb3fbdd2d951432a7d2f6c9ba96509892ead
Sat Garcia <sat@sandiego.edu>
2026-03-12 00:15:07 -0700

    add skeleton file for tic tac toe module

---
+++ tic_tac_toe.py
@@ -0,0 +1,14 @@
+"""
+Module: tic_tac_toe
+
+Implementation of the class two player game.
+"""
+
+# TODO: Add your code below this line
+
+
+def main():
+    print("🎵How much is that kitty in the window?🎵")
+
+if __name__ == "__main__":
+    main()
